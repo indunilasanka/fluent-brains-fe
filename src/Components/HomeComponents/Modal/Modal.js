2ebe24b0783e77d781e3a16a08e73792b2e693fb
Dileepa97 <54783169+Dileepa97@users.noreply.github.com>
2021-09-24 20:51:51 +0530

    bug-fix-issue-4-titlebar-and-scrolling-issue

--- src/Components/HomeComponents/Modal/Modal.js
+++ src/Components/HomeComponents/Modal/Modal.js
@@ -4,6 +4,7 @@ import Iframe from "react-iframe";
 import { IoMdClose } from "react-icons/io";
 import { useMainContext } from "../../../Context/Context";
 import mobile from "../../../images/Home/mobile.gif";
+
 const Wrapper = styled.div`
   position: fixed;
   top: 50%;
@@ -14,7 +15,6 @@ const Wrapper = styled.div`
   height: 102%;
   right: 0px;
   background: rgba(0, 0, 0, 0.8);
-  cursor: pointer;
 
   ::-webkit-scrollbar {
     display: none;
@@ -25,46 +25,50 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   z-index: 5000000000000000;
-  overflow: hidden;
+  // overflow: hidden;
 
   .modal-container {
     width: 960px;
-    height: 680px;
+    height: 670px; //change to control height
     display: flex;
     justifycontent: center;
     aling-items: center;
     flex-direction: column;
     z-index: 5 !important;
   }
+
   .modal-container ::-webkit-scrollbar {
     display: none;
   }
 
   .m-title {
-    background: #000;
+    background: #f7f4f2;
     width: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 25px 20px;
-    color: #fff;
+    padding: 15px 20px;
+    color: black;
     margin-top: 10px;
+    font-size: 18px;
+    font-weight: bold;
   }
+
   .close {
     margin: 0;
+    padding: 1px;
     cursor: pointer;
+    background: orange;
+    border-radius: 50%;
   }
+
   .mobile {
     display: none;
     height: 100%;
     padding-top: 38%;
     padding-left: 1%;
   }
-  @media only screen and (max-width: 500px) {
-    .m-title {
-      padding: 25px 20px;
-    }
-  }
+
   @media only screen and (max-width: 350px) {
     .mobile {
       display: block;
@@ -77,10 +81,17 @@ const Wrapper = styled.div`
       height: 100%;
     }
     .m-title {
-      padding: 25px 20px;
+      padding: 25px 40px;
+    }
+  }
+
+  @media only screen and (max-width: 300px) {
+    .m-title {
+      padding: 25px 50px;
     }
   }
 `;
+
 const Modal = () => {
   const { setModals } = useMainContext();
 
@@ -88,16 +99,18 @@ const Modal = () => {
     <Wrapper onClick={() => setModals(false)}>
       <div className="modal-container">
         <div className="m-title">
-          <h4>Here will be your title</h4>
+          <p>Here will be your title</p>
           <IoMdClose
             className="close"
-            size="25"
+            size="20"
             onClick={() => setModals(false)}
           />
         </div>
+
         <div className="mobile">
           <img src={mobile} alt="" />
         </div>
+
         <Iframe
           url="http://activities.fluentbrains.com.s3-website.ap-south-1.amazonaws.com/tense/"
           scrolling="no"
